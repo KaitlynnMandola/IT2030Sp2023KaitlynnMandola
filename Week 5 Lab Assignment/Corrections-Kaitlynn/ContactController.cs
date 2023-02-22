dbc5036193623457b69c94cfcdb080d5e5610bc6
FolusoFayomi <foluso.fayomi@tri-c.edu>
2023-02-22 15:20:11 -0500

    Add files via upload
    These files contain corrections for your lab. These are the files that required changes.

---
+++ Week 5 Lab Assignment/Corrections-Kaitlynn/ContactController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+//error caused because the following was missing. It was causing an error on line 15
+using KaitlynnKeyboardsAndKilobytes.Models;
+
+namespace KaitlynnKeyboardsAndKilobytes.Controllers
+{
+    public class ContactController : Controller
+    {
+        //missing [HttpGet] attribute
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //ViewBag.FV = 0;  this line is not needed, you can remove it
+            return View();
+        }
+
+        //missing [HttpPost] attribute
+        [HttpPost]
+        public IActionResult Index(ContactModel model)
+        {
+            //ViewBag.FV = model.ContactModel();  this line is not needed, you can remove it
+            return View(model);
+        }
+    }
+}
